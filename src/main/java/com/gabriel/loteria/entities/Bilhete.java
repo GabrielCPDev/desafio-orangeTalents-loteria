74e72363306af033c2bfce2420d04995832018e2
Gabriel Carvalho <gabrielcpcontato@outlook.com>
2021-02-01 17:27:35 -0300

    Criação de entidades e mapeamento relacional

--- src/main/java/com/gabriel/loteria/entities/Bilhete.java
+++ src/main/java/com/gabriel/loteria/entities/Bilhete.java
@@ -1,5 +1,111 @@
 package com.gabriel.loteria.entities;
 
-public class Bilhete {
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.ElementCollection;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_bilhetes")
+public class Bilhete implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private Instant dataAposta = Instant.now();
+	
+	@ManyToOne
+	@JoinColumn(name = "jogador_id")
+	private Jogador jogador;
+	private Integer numeroSorteio;
+	
+	@ElementCollection
+	private Set<Integer> numeros = new HashSet<>();
+	
+	public Bilhete () {
+		
+	}
+
+	public Bilhete(Long id, Instant dataAposta, Jogador jogador, Integer numeroSorteio) {
+		super();
+		this.id = id;
+		this.dataAposta = dataAposta;
+		this.jogador = jogador;
+		this.numeroSorteio = numeroSorteio;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Instant getDataAposta() {
+		return dataAposta;
+	}
+
+	public void setDataAposta(Instant dataAposta) {
+		this.dataAposta = dataAposta;
+	}
+
+	public Jogador getJogador() {
+		return jogador;
+	}
+
+	public void setJogador(Jogador jogador) {
+		this.jogador = jogador;
+	}
+
+	public Integer getNumeroSorteio() {
+		return numeroSorteio;
+	}
+
+	public void setNumeroSorteio(Integer numeroSorteio) {
+		this.numeroSorteio = numeroSorteio;
+	}
+
+	public Set<Integer> getNumeros() {
+		return numeros;
+	}
+
+	public void setNumeros(Set<Integer> numeros) {
+		this.numeros = numeros;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Bilhete other = (Bilhete) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 }
